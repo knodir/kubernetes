2bdbd7f31f68a3ac32ea71f065898eb7c5ad5582
Nodir Kodirov <nodir.qodirov@gmail.com>
2015-05-20 12:20:14 -0700

    fixed R plotting code

--- VMC/NAT/native/measurements/nat_latency_plot.R
+++ VMC/NAT/native/measurements/nat_latency_plot.R
@@ -45,32 +45,47 @@ arrFromThirdRun = eachRun("real_data/packet_latency3.txt", "real_data/nat_map_ca
 # get the min number of NAT entries for each run and trim array size to be the same, i.e., [0:min]
 minLength = min(length(arrFromFirstRun), length(arrFromSecondRun), length(arrFromThirdRun))
 
-
-sprintf("length(arrFromFirstRun) = %f", length(arrFromFirstRun))
-sprintf("length(arrFromSecondRun) = %f", length(arrFromSecondRun))
-sprintf("length(arrFromThirdRun) = %f", length(arrFromThirdRun))
+#sprintf("length(arrFromFirstRun) = %f", length(arrFromFirstRun))
+#sprintf("length(arrFromSecondRun) = %f", length(arrFromSecondRun))
+#sprintf("length(arrFromThirdRun) = %f", length(arrFromThirdRun))
 
 sprintf("minLength = %f", minLength)
 
-trimmedFirst = NULL
-trimmedSecond = NULL
-trimmedThird = NULL
+#trimmedFirst = NULL
+#trimmedSecond = NULL
+#trimmedThird = NULL
 
-for (i in 1:length(minLength)) trimmedFirst[i] = arrFromFirstRun[i];
-for (i in 1:length(minLength)) trimmedSecond[i] = arrFromSecondRun[i];
-for (i in 1:length(minLength)) trimmedThird[i] = arrFromThirdRun[i];
+#for (i in 1:length(minLength)) trimmedFirst[i] = arrFromFirstRun[i];
+#for (i in 1:length(minLength)) trimmedSecond[i] = arrFromSecondRun[i];
+#for (i in 1:length(minLength)) trimmedThird[i] = arrFromThirdRun[i];
+
 
 # final array with the average value of the three runs. This will be plotted.
-# avgArr = trimmedFirst
-# avgArr = (trimmedFirst + trimmedSecond)/2
-avgArr = (trimmedFirst + trimmedSecond + trimmedThird)/3
-
+avgArr = (arrFromFirstRun + arrFromSecondRun + arrFromThirdRun)/3
 # sprintf("%f", avgArr)
+
+# select point we would like to draw on the graph
+step = 1000
+plotPoints = minLength / step
+
+natCap = NULL
+laten = NULL 
+finalArr = NULL
+
+for (i in 1:plotPoints) {
+    natCap[i] = i*step
+    laten[i] = avgArr[i*step]
+}
+
+finalArr = cbind(natCap, laten)
+
+sprintf("finalArr = %f", finalArr)
 
 png('latency.png', width = 6, height=5, units="in", res=300)
 
-plot(avgArr, col="blue", xlab="NAT capacity", ylab="Packet delay (ms)")
-# plot(avgArr, type="o", col="blue", xlab="NAT capacity", ylab="Packet delay (ms)")
+# plot(avgArr, col="blue", xlab="NAT capacity", ylab="Packet delay (ms)")
+plot(finalArr[1], finalArr[2], type="o", col="blue", xlab="NAT capacity", ylab="Packet delay (ms)")
+
 # lines(fw2time, fw2rampct, type="o", col="red")
 # lines(fw3time, fw3rampct, type="o", col="green")
 # lines(fw4time, fw4rampct, type="o", col="black")
@@ -80,3 +95,13 @@ title(main="Latency")
 
 grid(col="gray")
 dev.off()
+
+
+# [1] "finalArr = 1000.000000" "finalArr = 2000.000000" "finalArr = 3000.000000"
+# [4] "finalArr = 4000.000000" "finalArr = 5000.000000" "finalArr = 6000.000000"
+# [7] "finalArr = 7000.000000" "finalArr = 8000.000000" "finalArr = 9000.000000"
+#[10] "finalArr = 2.723317"    "finalArr = 2.080749"    "finalArr = 8.658681"   
+#[13] "finalArr = 4.078346"    "finalArr = 3.482432"    "finalArr = 5.955279"   
+#[16] "finalArr = 7.726355"    "finalArr = 0.870694"    "finalArr = 4.214192" 
+
+
